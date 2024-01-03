55c0864b7d74cd27a51e9cb73df69cb73efbf970
vaibhav-rahinj <rahinjvaibhav123@gmail.com>
2024-01-03 11:31:00 +0530

    config file for db

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`connected to mongo database ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error in mongodb ${error}`)
+    }
+}
+export default connectDB;
