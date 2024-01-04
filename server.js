e18c16867f5aa1f4c24cb0b8bd7a1f99e09fb401
vaibhav-rahinj <70082433+vaibhav-rahinj@users.noreply.github.com>
2024-01-04 13:33:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js"
 import categoryRoutes from './routes/categoryRoutes.js'
 import cors from 'cors'
 import path from 'path'
-
+import { fileURLToPath } from 'url';
 const app = express();
 
 // .env configration 
@@ -16,6 +16,10 @@ dotenv.config();
 
 // database config 
 connectDB();
+
+// esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 // middlewares
 app.use(cors());
 app.use(express.json());
